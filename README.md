73a4168a58217282e4afb8fd397817e3a731955b
GeekinGH <chenhai@chenhaideMacBook-Air.local>
2024-01-15 16:19:42 +0800

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-# AiChatHelper
+# 微信助手 ChatGPT 反向代理
+
+欢迎来到微信助手 ChatGPT 反向代理项目！这个仓库帮助你在 replit.com 上部署 ChatGPT / Gemini-pro 反向代理，使其能够与懒猫的微信助手插件的 ChatGPT 功能协同工作。
+
+## Fork 仓库
+
+1. 点击仓库右上角的 "Fork" 按钮，创建你自己的分支。
+
+## 部署到 replit
+
+1. 注册[Replit 帐号](https://replit.com/)。
+2. 在 Replit 上创建一个新项目(Create Node.js)。
+3. 点击弹出的窗口右上角 "Import from GitHub" 。
+4. Connect your GitHub account-->Install&Authorize.
+5. 将你 Fork 的 GitHub 仓库连接到你的 Replit 项目，项目名任意。
+6. Replit 将自动检测到你的 Node.js 应用并设置必要的配置。
+
+## 设置环境变量
+
+1. 在 Replit 项目中，进入 "Tools" 选项卡。
+2. 找到 "Secrets" 部分。
+3. 添加以下环境变量(+ New Secret)：
+   - `MAX_RETRIES`：设置最大重试次数。选填项，不添加此变量则默认1，即可重试一次，也就是会执行 2 次。
+   - `WXID_ARRAY`：设置授权的 WXID 数组（以逗号分隔）。选填项，不添加此变量则默认为空，表示不进行授权验证。
+   - `CHATGPT_TEMPERATURE`：设置 ChatGPT 的温度。选填项，不添加此变量则默认为 1，从0开始，温度值越高，生成的文本越随机和创造性，而温度值越低，生成的文本越保守和可预测。
+
+## 运行应用
+
+1. 一旦项目设置完成，Replit 上的 "My Repls" 选项卡选择你刚刚创建的项目。
+2. 点击 "Run" 运行应用。
+3. 应用启动后，“Webview”选项卡点击“New tab”，打开默认浏览器，复制https地址链接，这就是代理服务器的地址，不需要加端口。
+
+## 使用方法
+以下操作都是在“微信助手”ChatGPT中操作：
+1. 将你的代理地址填写到“代理地址”栏。
+2. 如果使用的是ChatGPT API，请和 “APIKey”中填写ChatGPT(Openai)的API Key，在“模型”中选择对应的gpt-4或者gpt-3.5-turbo。
+3. 如果使用的是Gemini-pro API，请和 “APIKey”中填写Gemini-pro的API Key，在“模型”中选择 手动输入 ，填写：Gemini 或者 Gemini-pro！
+
+## 其他注意事项
+- 代码修改自懒猫提供的Gemini.zip，[懒猫插件交流](https://t.me/maogroup)
+- 如果遇到任何问题，请参考[Replit 文档](https://docs.replit.com)进行故障排除。
+- 有关 ChatGPT 相关配置，请查看你的仓库中的 app.js 文件。
+
+祝你在微信助手中体验愉快！
---
+++ index.js
@@ -0,0 +1,196 @@
+// 导入所需的模块
+const express = require('express');
+const axios = require('axios');
+
+// 创建 Express 应用
+const app = express();
+const port = 3000; // 使用Replit提供的端口
+
+// 从环境变量获取最大重试次数，默认为 1
+const maxRetries = process.env.MAX_RETRIES ? parseInt(process.env.MAX_RETRIES) : 1;
+
+// 计数器和 API Key
+let retryCount = 0;
+let apikey = '';
+
+// 错误处理函数
+function handleRetryableError(res, error, errorMessage, retryFunction) {
+    if (retryCount < maxRetries) {
+        // 等待2秒后重试
+        setTimeout(() => {
+            retryCount++;
+            retryFunction();
+        }, 2000);
+    } else {
+        respondWithError(res, errorMessage);
+    }
+}
+
+// 发送错误响应
+function respondWithError(res, messages) {
+    res.json({
+        'choices': [{
+            'message': {
+                'role': 'assistant',
+                'content': messages
+            }
+        }]
+    });
+}
+
+// 使用 JSON 解析中间件
+app.use(express.json());
+
+// 处理请求的主要逻辑
+app.use('/', async (req, res) => {
+    try {
+        // 使用环境变量进行验证
+        const wxidArray = process.env.WXID_ARRAY ? process.env.WXID_ARRAY.split(',') : [];
+        const wxid = req.headers['wxid'];
+
+        // 判断 wxidArray 是否为空，如果为空则不进行授权验证，直接执行后续程序
+        if (wxidArray.length > 0 && !wxidArray.includes(wxid)) {
+            respondWithError(res, '我是狗，偷接口，偷了接口当小丑～');
+            return;
+        }
+
+        const messages = req.body.messages;
+        if (!messages) {
+            respondWithError(res, '出错啦，请添加到插件的反代中使用！');
+            return;
+        }
+
+        // 初始化失败重试计数器
+        retryCount = 0;
+
+        const chat_model = req.body.model.trim().toLowerCase();
+        if (chat_model == 'gpt-3.5-turbo' || chat_model == 'gpt-4') {
+            // 对接 ChatGPT 的 API
+            await handleChatGPTAPIRequest(req, res, chat_model);
+        } else if (chat_model == 'gemini-pro' || chat_model == 'gemini') {
+            const authorization = req.headers['authorization'];
+            apikey = authorization.slice(7);
+
+            // 格式化 Gemini 的消息
+            const formattedMessages = formatMessagesForGemini(messages);
+            // 对接 Gemini 的 API
+            await processGeminiRequest(formattedMessages, res);
+        } else {
+            respondWithError(res, '不支持的 chat_model 类型');
+        }
+    } catch (error) {
+        respondWithError(res, error.toString());
+    }
+});
+
+// 处理 ChatGPT 请求
+async function handleChatGPTAPIRequest(req, res, chat_model) {
+    const API_KEY = req.headers['authorization'];
+    const url = 'https://api.openai.com/v1/chat/completions';
+
+    try {
+        const response = await axios({
+            method: req.method,
+            url: url,
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': API_KEY,
+            },
+            data: {
+                'model': chat_model,
+                'messages': req.body.messages,
+                'temperature': process.env.CHATGPT_TEMPERATURE ? parseFloat(process.env.CHATGPT_TEMPERATURE) : 1
+            }
+        });
+
+        res.json(response.data);
+    } catch (error) {
+        handleRetryableError(res, error, `ChatGPT请求出错: ${error.toString()}`, () => handleChatGPTAPIRequest(req, res, chat_model));
+    }
+}
+
+// 格式化符合 Gemini 的 messages
+function formatMessagesForGemini(messages) {
+    let formattedMessages = [];
+
+    messages.forEach((item, index) => {
+        // 插件传递过来的第一条消息是预设，Gemini第一条消息要求是用户，所以当设置预设后添加上一条模型回复的内容
+        if (index === 0) {
+            formattedMessages.push({
+                'role': 'user',
+                'parts': [{
+                    'text': item.content
+                }]
+            }, {
+                'role': 'model',
+                'parts': [{
+                    'text': '好的'
+                }]
+            });
+        } else if (index === 1 && item.role === 'assistant') {
+            // 由于Gemini接收对话的格式必须是user/model/user/model...，多条对话时插件传递过来的第二条可能会变成assistant所以忽略掉
+        } else {
+            if (item.role === 'assistant') {
+                formattedMessages.push({
+                    'role': 'model',
+                    'parts': [{
+                        'text': item.content
+                    }]
+                });
+            } else {
+                formattedMessages.push({
+                    'role': 'user',
+                    'parts': [{
+                        'text': item.content
+                    }]
+                });
+            }
+        }
+    });
+
+    return formattedMessages;
+}
+
+// 处理 Gemini 请求
+async function processGeminiRequest(contents, res) {
+    try {
+        const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=${apikey}`;
+
+        const response = await axios.post(url, {
+            'contents': contents,
+            "safetySettings": [{
+                "category": "HARM_CATEGORY_HARASSMENT",
+                "threshold": "BLOCK_NONE"
+            }, {
+                "category": "HARM_CATEGORY_HATE_SPEECH",
+                "threshold": "BLOCK_NONE"
+            }, {
+                "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+                "threshold": "BLOCK_NONE"
+            }, {
+                "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+                "threshold": "BLOCK_NONE"
+            }]
+        });
+
+        if (response.data.candidates) {
+            res.json({
+                'choices': [{
+                    'message': {
+                        'role': 'assistant',
+                        'content': response.data.candidates[0].content.parts[0].text
+                    }
+                }]
+            });
+        } else {
+            handleRetryableError(res, new Error('Gemini请求出错'), 'Gemini请求出错', () => processGeminiRequest(contents, res));
+        }
+    } catch (error) {
+        handleRetryableError(res, error, `Gemini请求出错: ${error.toString()}`, () => processGeminiRequest(contents, res));
+    }
+}
+
+// 启动 Express 应用
+app.listen(port, () => {
+    console.log(`服务启动成功 http://localhost:${port}`);
+});
