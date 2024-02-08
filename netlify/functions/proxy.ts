6e94cb52627c464538a398a494ac27a019dbed53
GeekinGH <111835669+GeekinGH@users.noreply.github.com>
2024-02-09 00:50:57 +0800

    为了更好扩展和迁移，把各API抽象化，功能无改变，可以不更新。

--- netlify/functions/proxy.ts
+++ netlify/functions/proxy.ts
@@ -1,4 +1,7 @@
 import { Context } from "@netlify/edge-functions";
+import Gemini from "./Gemini";
+import ChatGPT from "./ChatGPT";
+import Qwen from "./Qwen";
 
 // 从 Netlify 的环境变量中获取授权的微信ID
 const wxidArray = process.env.WXID_ARRAY ? process.env.WXID_ARRAY.split(',') : [];
@@ -7,12 +10,11 @@ const wxidArray = process.env.WXID_ARRAY ? process.env.WXID_ARRAY.split(',') : [
 function respondJsonMessage(message) {
     const jsonMessage = {
         choices: [{
-                message: {
-                    role: 'assistant',
-                    content: message,
-                },
-            }
-        ],
+            message: {
+                role: 'assistant',
+                content: message,
+            },
+        }],
     };
 
     return new Response(JSON.stringify(jsonMessage), {
@@ -22,16 +24,15 @@ function respondJsonMessage(message) {
     });
 }
 
-export default async(request: Request, context: Context) => {
+export default async (request: Request, context: Context) => {
     try {
         const wxid = request.headers.get("wxid");
         if (!wxid) {
             throw new Error('未提供 wxid 头部信息');
         }
-        const chatAuthorization = request.headers.get("authorization");
-        const apiKey = chatAuthorization ? chatAuthorization.replace('Bearer ', '') : '';
+        const requestAuthorization = request.headers.get("authorization");
         const requestBody = await request.json();
-        const chatModel = requestBody.model.toLowerCase().trim();
+        const requestModel = requestBody.model.toLowerCase().trim();
         const countryName = context.geo?.country?.name || "somewhere in the world";
 
         // 判断 wxidArray 是否为空，如果为空则不进行授权验证，直接执行后续程序
@@ -39,179 +40,33 @@ export default async(request: Request, context: Context) => {
             return respondJsonMessage('我是狗，偷接口，偷了接口当小丑～');
         }
 
-        if (chatModel === 'gpt-3.5-turbo' || chatModel === 'gpt-4') {
-            return handleChatGPTRequest(requestBody, chatModel, chatAuthorization);
-        } else if (chatModel === 'gemini-pro' || chatModel === 'gemini') {
-            return handleGeminiRequest(requestBody, chatModel, apiKey);
-        } else if (chatModel === 'qwen-turbo' || chatModel === 'qwen-max') {
-            return handleQwenRequest(requestBody, chatModel, chatAuthorization);
+        let response;
+        if (requestModel === 'gpt-3.5-turbo' || requestModel === 'gpt-4') {
+            const chatGPT = new ChatGPT(requestModel, requestAuthorization, requestBody.messages);
+            response = await chatGPT.handleResponse(await getResponse(chatGPT.url, 'POST', chatGPT.headers, chatGPT.body));
+        } else if (requestModel === 'gemini-pro' || requestModel === 'gemini') {
+            const gemini = new Gemini(requestModel, requestAuthorization, requestBody.messages);
+            response = await gemini.handleResponse(await getResponse(gemini.url, 'POST', gemini.headers, gemini.body));
+        } else if (requestModel === 'qwen-turbo' || requestModel === 'qwen-max') {
+            const qwen = new Qwen(requestModel, requestAuthorization, requestBody.messages);
+            response = await qwen.handleResponse(await getResponse(qwen.url, 'POST', qwen.headers, qwen.body));
         } else {
             return respondJsonMessage('不支持的 chat_model 类型');
         }
+
+        return respondJsonMessage(response);
     } catch (error) {
+        console.error('Error:', error); // 记录错误信息
         return respondJsonMessage(`出错了: ${error.toString()}`);
     }
 }
 
-async function handleChatGPTRequest(requestBody, chatModel, apiKey) {
-    const url = 'https://api.openai.com/v1/chat/completions';
-    try {
-        const response = await fetch(url, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': apiKey,
-            },
-            body: JSON.stringify({
-                'model': chatModel,
-                'messages': requestBody.messages,
-            }),
-        });
-        const responseData = await response.json();
-
-        // 判断是否有错误信息
-        if (responseData.error) {
-            // 处理错误逻辑
-            const errorMessage = responseData.error.message || '未知错误';
-            return respondJsonMessage(`${chatModel}: ${errorMessage}`);
-        }
-
-        // 如果没有错误，返回 ChatGPT API 的响应
-        return new Response(JSON.stringify(responseData));
-    } catch (error) {
-        return respondJsonMessage(`${chatModel}: ${error.toString()}`);
-    }
-}
-
-function formatMessagesForGemini(messages) {
-    let formattedMessages = [];
-    messages.forEach((item, index) => {
-        if (index === 0) {
-            formattedMessages.push({
-                'role': 'user',
-                'parts': [{
-                        'text': item.content,
-                    }
-                ],
-            }, {
-                'role': 'model',
-                'parts': [{
-                        'text': '好的',
-                    }
-                ],
-            });
-        } else if (index === 1 && item.role === 'assistant') {
-            // 忽略掉第二条消息
-        } else {
-            formattedMessages.push({
-                'role': (item.role === 'assistant') ? 'model' : 'user',
-                'parts': [{
-                        'text': item.content,
-                    }
-                ],
-            });
-        }
+async function getResponse(url, method, headers, body) {
+    const response = await fetch(url, {
+        method: method,
+        headers: headers,
+        body: JSON.stringify(body),
     });
-    return formattedMessages;
-}
-
-async function handleGeminiRequest(requestBody, chatModel, apiKey) {
-    const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=${apiKey}`;
-    try {
-        const chatMessages = requestBody.messages;
-        const contents = formatMessagesForGemini(chatMessages);
-        const response = await fetch(url, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                'contents': contents,
-                "safetySettings": [{
-                        "category": "HARM_CATEGORY_HARASSMENT",
-                        "threshold": "BLOCK_NONE"
-                    }, {
-                        "category": "HARM_CATEGORY_HATE_SPEECH",
-                        "threshold": "BLOCK_NONE"
-                    }, {
-                        "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
-                        "threshold": "BLOCK_NONE"
-                    }, {
-                        "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
-                        "threshold": "BLOCK_NONE"
-                    }
-                ],
-            }),
-        });
-
-        const responseData = await response.json();
-        if (responseData.candidates) {
-            return respondJsonMessage(responseData.candidates[0].content.parts[0].text);
-        } else {
-            // 处理失败逻辑，查看是否有错误消息字段
-            const errorMessage = responseData.error ? responseData.error.message : '未知错误';
-            return respondJsonMessage(`${chatModel} : ${errorMessage}`);
-        }
-    } catch (error) {
-        return respondJsonMessage(`${chatModel}: ${error.toString()}`);
-    }
-}
-
-async function handleQwenRequest(requestBody, chatModel, apiKey) {
-    const url = 'https://dashscope.aliyuncs.com/api/v1/services/aigc/text-generation/generation';
-    try {
-        const chatMessages = requestBody.messages;
-        const contents = formatMessagesForQwen(chatMessages);
-        const response = await fetch(url, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': apiKey,
-            },
-            body: JSON.stringify({
-                'model': chatModel,
-                'input': {
-                    'messages': contents,
-                },
-                'parameters': {},
-            }),
-        });
-        const responseData = await response.json();
-        // 判断是否有错误信息
-        if (responseData.code) {
-            // 处理错误逻辑
-            const errorCode = responseData.code;
-            const errorMessage = responseData.message || '未知错误';
-            return respondJsonMessage(`${chatModel}: ${errorCode} - ${errorMessage}`);
-        }
-
-        // 如果没有错误，返回 Qwen API 的响应
-        return respondJsonMessage(responseData.output.text);
-    } catch (error) {
-        return respondJsonMessage(`捕获错误${chatModel}: ${error.toString()}`);
-    }
-}
-
-function formatMessagesForQwen(messages) {
-    let formattedMessages = [];
-    messages.forEach((item, index) => {
-        if (index === 0 && item.role === 'system') {
-            let itemContent = item.content.trim();
-            if(itemContent ==""){
-                itemContent = '你是通义千问';
-            }
-            formattedMessages.push({
-                'role': 'system',
-                'content': itemContent,
-            });
-        } else if (index === 1 && item.role === 'assistant') {
-            // 忽略掉第二条消息
-        } else {
-            formattedMessages.push({
-                'role': (item.role === 'assistant') ? 'assistant' : 'user',
-                'content': item.content,
-            });
-        }
-    });
-    return formattedMessages;
+    const responseData = await response.json();
+    return responseData;
 }
