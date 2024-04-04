5233a380b8d443b5129b49cf14b1526ba8823769
GeekinGH <111835669+GeekinGH@users.noreply.github.com>
2024-04-04 12:39:39 +0800

    Bug fixed

--- netlify/functions/Gemini.ts
+++ netlify/functions/Gemini.ts
@@ -9,11 +9,11 @@ export default class Gemini {
     constructor(requestModel: string, requestAuthorization: string, requestMessages: any) {
         this.model = requestModel;
         this.authorization = requestAuthorization ? requestAuthorization.replace('Bearer ', '') : '';
-        if (this.model === "gmini") {
-            this.url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=${this.authorization}`;
-        } else {
-            this.url = `https://generativelanguage.googleapis.com/v1beta/models/${this.model}:generateContent?key=${this.authorization}`;
+        if (this.model === "gemini") {
+            this.model = "gemini-pro";
         }
+        this.url = `https://generativelanguage.googleapis.com/v1beta/models/${this.model}:generateContent?key=${this.authorization}`;
+        
         this.formatHeaders();
         try {
             this.formatBody(requestMessages);
